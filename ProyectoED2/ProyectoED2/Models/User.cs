8f3ae6533bf3106a9c46f4fbd6ae3b5212166340
FranciscoJMorales <55027785+FranciscoJMorales@users.noreply.github.com>
2020-11-06 15:59:48 -0600

    Se creó el controlador y las vistas para hacer log in y registrarse

---
+++ ProyectoED2/ProyectoED2/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoED2.Models
+{
+    public class User
+    {
+        public string Nombre { get; set; }
+    }
+}
